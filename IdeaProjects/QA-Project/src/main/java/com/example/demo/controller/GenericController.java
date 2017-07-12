6cd53e42ef75a0941d080f93fb7d540b7f443e2f
Spectre <englbery@fim.uni-passau.de>
2017-07-12 10:10:10 +0200

    DBInit für den Datensatz. (Übernimmt auch UserInit somit überflüssig) GenericController übernimmt das Löschen des Init Datensatzes per URL Answer und Question Länge

--- IdeaProjects/QA-Project/src/main/java/com/example/demo/controller/GenericController.java
+++ IdeaProjects/QA-Project/src/main/java/com/example/demo/controller/GenericController.java
@@ -1,6 +1,46 @@
 package com.example.demo.controller;
 
+import com.example.demo.application.intit.DBInit;
+import com.example.demo.persistence.repository.AnswerRepository;
+import com.example.demo.persistence.repository.QuestionRepository;
+import com.example.demo.persistence.repository.RoleRepository;
+import com.example.demo.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GenericController {}
+public class GenericController {
+
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private boolean toggledInit = false;
+
+    @Autowired
+    public GenericController(QuestionRepository questionRepository, AnswerRepository answerRepository, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.questionRepository = questionRepository;
+        this.answerRepository = answerRepository;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @RequestMapping(value = "/deleteInit")
+    public String deleteInit() {
+        if (toggledInit == false) {
+            this.questionRepository.deleteAll();
+            this.answerRepository.deleteAll();
+            this.userRepository.deleteAll();
+            toggledInit = true;
+            return "Sucessfull deleted! Please refresh QA site!";
+        } else {
+            return "Error! You already deleted the init data!";
+        }
+
+    }
+
+}
